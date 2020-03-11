9c964c46892e02179c368b8632e11ae28ac48621
sharathsamala <sharathsamala@live.com>
2020-03-11 23:12:22 +0530

    added permissions to profiles view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,8 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import models, serializers
+from profiles_api import permissions
 
 
 class HelloApiView(APIView):
@@ -22,9 +24,10 @@ class HelloApiView(APIView):
 
 class UserProfileViewSet(viewsets.ModelViewSet):
     """Handle creating and updating profiles"""
-
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfiles,)
 
 
 
